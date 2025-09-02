72b5219fb62a5fed816ea533c4ff9d9e52eccffc
Elvandra282 <3lvandra282@gmail.com>
2025-09-02 20:54:30 +0700

    2902575274

---
+++ FransiscusElvandraTjutju.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+int Umur = 18;
+int Kelahiranku = 2029 - 02 - 20;
+int BulanLahir = 02;
+int TanggalLahir = 28;
+
+printf("Hi Everyone, My Name Is Fransiscus Elvandra Tjutju\n"); //Introduction
+printf("Guys You Can Call Me Elvan, Elvandra, or Cucu\n"); 
+printf("My Favourite Programming Languange is C For Right Now\n");  //Favourite Programming Languange
+
+printf("Umurku Saat Ini Adalah = %d\n", Umur); //Umurku
+printf("Aku Lahir Pada Tahun = %d\n", Kelahiranku);
+printf("Aku Lahir Pada Bulan = %d\n", BulanLahir);
+printf("Aku Lahir Pada Tanggal = %d\n", TanggalLahir);
+
+return 0;
+}
